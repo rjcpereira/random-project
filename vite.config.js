3aefd24d458c40e82071d09a02dcb84c3fe9189b
Ricardo Pereira <rjcpereira@outlook.pt>
2023-05-19 09:52:53 +0100

    import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
+import { legacy } from 'sp-dev-x-kit'
+
+export default legacy(import.meta.url);
+
+//same as: 
+
+/* 
 import xKit from 'sp-dev-x-kit'
 
 export default xKit(import.meta.url, {
     legacy: true
 });
+*/
